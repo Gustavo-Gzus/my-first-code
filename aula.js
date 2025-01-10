8b5bd70f8f9261a891f4b933c5ab405f05d0180a
Gustavo Silva <gustavojesussilva800@gmail.com>
2025-01-10 14:31:03 -0300

    my first code

---
+++ aula.js
@@ -0,0 +1,10 @@
+var demografia = [
+    "população",254857,
+"Área",1136907]
+
+var denpopu = demografia[1]
+var denpopu2 = demografia[3]
+var result = demografia[1]/demografia[3]
+console.log(`A densidade é de ${result}`)
+console.log(demografia+[",densidade", result])
+
